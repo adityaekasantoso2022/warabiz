7869ff53d1cb48092c3fa12e38e8e85fd3e3adb6
adityaekasantoso12 <adityaekasantoso12@gmail.com>
2024-02-12 11:29:13 +0700

    bantuan

--- resources/views/pages/user/bantuan.blade.php
+++ resources/views/pages/user/bantuan.blade.php
@@ -71,16 +71,28 @@
             background: #fff;
             border: none;
             border-radius: 14px;
-            box-sizing: border-box;
             color: #34364a;
             display: flex;
             flex-direction: column;
             height: 100%;
+            width: 99%;
             padding: 30px;
-            position: relative;
+            position: center;
             row-gap: 24px;
         }
 
+        .card-row {
+            background: #fff;
+            border-radius: 10px;
+            box-sizing: border-box;
+            color: #34364a;
+            height: 100%;
+            weight: 100px;
+            padding: 30px;
+            position: relative;
+            row-gap: 20px;
+
+        }
 
         /* Styling untuk kartu */
         .card {
@@ -101,7 +113,7 @@
     <section class="py-5" style="margin-top: 10px">
         <div class="container">
             <div class="mt-5 row pricing testimonials mentors checkout gy-4" id="reviews">
-                <div class="bantuan-card">
+                <div class="bantuan-card" style="margin-bottom: 20px;">
                     <div class="row justify-content-center mt-1">
                         <div class="col-md-3 col-xs-12 mb-3">
                             <div class="card text-center rounded-card">
@@ -117,7 +129,7 @@
                                 <div class="card-body">
                                     <img src="{{ asset('assets/frontend/image/icon/gmail.png') }}">
                                     <h6>Email</h6>
-                                    <p>support@warabiz.com</p>
+                                    <p>bantuan@warabiz.com</p>
                                 </div>
                             </div>
                         </div>
@@ -133,7 +145,8 @@
                         <div class="col-md-3 col-xs-12 mb-3">
                             <div class="card text-center rounded-card">
                                 <div class="card-body">
-                                <img src="{{ asset('assets/frontend/image/icon/instagram.png') }}" style="width: 100px; height: auto;">
+                                    <img src="{{ asset('assets/frontend/image/icon/instagram.png') }}"
+                                        style="width: 100px; height: auto;">
                                     <h6>Instagram</h6>
                                     <p>@warabiz.com</p>
                                 </div>
@@ -141,46 +154,57 @@
                         </div>
                     </div>
                 </div>
-                <div class="bantuan-card">
-                    <h5 class="header-title mb-0">
-                        <b>Form Pertanyaan</b>
-                    </h5>
-                    <form id="myForm" action="{{ route('submit.form') }}" method="POST">
-                        @csrf
-                        <div class="form-group mt-1">
-                            <label for="payment_method" class="form-label fw-bold">
-                                <p>Pilih Kategori Pertanyaan <span style="color: red;">*</span></p>
-                            </label>
-                            <select name="kategori_pertanyaan" id="kategori_pertanyaan"
-                                class="form-select border px-2 py-1 rounded-3 shadow-none" required>
-                                <option value="" selected disabled>Pilih Kategori</option>
-                                <option value="Informasi Produk">Informasi Produk</option>
-                                <option value="Proses Pembelian">Proses Pembelian</option>
-                                <option value="Layanan Pelanggan">Layanan Pelanggan</option>
-                                <option value="Lainnya">Lainnya</option>
-                            </select>
-                            <p class="text-sm text-secondary mt-2">
-                                Pilih kategori pertanyaan
-                            </p>
-                        </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="card-row">
+                    <h5 class="header-title">
+                            <b>Pertanyaan Populer</b>
+                        </h5>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card-row">
+                        <h5 class="header-title">
+                            <b>Form Pertanyaan</b>
+                        </h5>
+                        <form id="myForm" action="{{ route('submit.form') }}" method="POST">
+                            @csrf
+                            <div class="form-group mt-1">
+                                <label for="payment_method" class="form-label fw-bold">
+                                    <p>Pilih Kategori Pertanyaan <span style="color: red;">*</span></p>
+                                </label>
+                                <select name="kategori_pertanyaan" id="kategori_pertanyaan"
+                                    class="form-select border px-2 py-1 rounded-3 shadow-none" required>
+                                    <option value="" selected disabled>Pilih Kategori</option>
+                                    <option value="Informasi Produk">Informasi Produk</option>
+                                    <option value="Proses Pembelian">Proses Pembelian</option>
+                                    <option value="Layanan Pelanggan">Layanan Pelanggan</option>
+                                    <option value="Lainnya">Lainnya</option>
+                                </select>
+                                <p class="text-sm text-secondary mt-2">
+                                    Pilih kategori pertanyaan
+                                </p>
+                            </div>
 
-                        <div class="form-group mt-4">
-                            <label for="pesan" class="form-label fw-bold">
-                                <p>Pesan <span style="color: red;">*</span></p>
-                            </label>
-                            <textarea name="pesan" id="pesan"
-                                class="form-control border px-2 py-1 rounded-3 shadow-none" required cols="30"
-                                rows="3"></textarea>
-                            <p class="text-sm text-secondary mt-2">
-                                Masukan pertanyaan anda
-                            </p>
-                        </div>
-                        <br>
-                        <!-- Submit button -->
-                        <button type="submit" onclick="validateForm()"
-                            class="mt-4 mb-2 btn bgTheme w-100 text-white border-12 py-3" id="submitButton">
-                            Kirim</button>
-                    </form>
+                            <div class="form-group mt-4">
+                                <label for="pesan" class="form-label fw-bold">
+                                    <p>Pesan <span style="color: red;">*</span></p>
+                                </label>
+                                <textarea name="pesan" id="pesan"
+                                    class="form-control border px-2 py-1 rounded-3 shadow-none" required cols="30"
+                                    rows="3"></textarea>
+                                <p class="text-sm text-secondary mt-2">
+                                    Masukan pertanyaan anda
+                                </p>
+                            </div>
+                            <br>
+                            <!-- Submit button -->
+                            <button type="submit" onclick="validateForm()"
+                                class="mt-4 mb-2 btn bgTheme w-100 text-white border-12 py-3" id="submitButton">
+                                Kirim</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
